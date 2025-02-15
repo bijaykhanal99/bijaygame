43d2b87edf240a3d341d1e65fb88ba26a80a9f18
bijaykhanal99 <116628282+bijaykhanal99@users.noreply.github.com>
2025-02-15 19:20:07 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function back() {
+window.history.back();
+}
+function ntew(){
+  window.location.href = "new.html";
+}
+function neww() {
+  window.location.href = "neww.html";
+}
+function newww() {
+  window.location.href = "newww.html";
+}
+function birt() {
+  window.location.href = "https://bijaykhanal08.github.io/bijaykhanal088/";
+}
